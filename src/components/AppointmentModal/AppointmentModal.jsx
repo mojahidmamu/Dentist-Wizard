3b4906f2da415d39649d7a8f69d436b9ff78af6a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-08 07:14:28 +0600

    added new file and folder

--- src/components/AppointmentModal/AppointmentModal.jsx
+++ src/components/AppointmentModal/AppointmentModal.jsx
@@ -3,66 +3,53 @@ import React from "react";
 const AppointmentModal = () => {
   return (
     <div>
-      <div className="my-5 mx-3 mx-auto">
-        <div className="stats shadow">
-          <div className="stat">
-            <div className="stat-figure text-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title">Total Likes</div>
-            <div className="stat-value text-primary">25.6K</div>
-            <div className="stat-desc">21% more than last month</div>
-          </div>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn my-5 mb-36 ml-36 flex justify-center items-center"
+        onClick={() => document.getElementById("my_modal_5").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div>
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                <label className="fieldset-label">Name</label>
+                <input type="text" className="input" placeholder="Name" />
+                  <label className="fieldset-label">Email</label>
+                  <input type="email" className="input" placeholder="Email" />
+                  <label className="fieldset-label">Password</label>
+                  <label className="fieldset-label">Address</label>
+                  <input type="text" className="input" placeholder="Address" />
+                  <label className="fieldset-label">Appoinment Date</label>
+                  <input type="date" className="input" placeholder="Date" />
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                  />
 
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-8 w-8 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 10V3L4 14h7v7l9-11h-7z"
-                ></path>
-              </svg>
-            </div>
-            <div className="stat-title">Page Views</div>
-            <div className="stat-value text-secondary">2.6M</div>
-            <div className="stat-desc">21% more than last month</div>
-          </div>
-
-          <div className="stat">
-            <div className="stat-figure text-secondary">
-              <div className="avatar online">
-                <div className="w-16 rounded-full">
-                  <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                </div>
+                  <button className="btn btn-neutral mt-4">
+                    Make Appoinment
+                  </button>
+                </fieldset>
               </div>
             </div>
-            <div className="stat-value">86%</div>
-            <div className="stat-title">Tasks done</div>
-            <div className="stat-desc text-secondary">31 tasks remaining</div>
+          </div>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
           </div>
         </div>
-      </div>
-      
-       
+      </dialog>
     </div>
   );
 };
